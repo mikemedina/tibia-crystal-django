9fb2ce58f481484f6c5d4e6c065ae75d752afe29
Mike Medina <mikemedina805@gmail.com>
2023-05-31 23:57:32 -0400

    Improve the spell admin page

--- crystal/spells/admin.py
+++ crystal/spells/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Spell
 
-admin.site.register(Spell)
+
+class SpellAdmin(admin.ModelAdmin):
+    list_display = ['name', 'vocation', 'x_max', 'y_max', 'x_min', 'y_min',
+              'target_cap']
+
+
+admin.site.register(Spell, SpellAdmin)
